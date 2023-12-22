46b47fe72ce83cc7a407fa547498fa9e85f5aca6
Leandro Souza <leandro.souza.159@gmail.com>
2023-12-22 14:18:34 -0300

    Ajuste do visual das métricas

--- Dashboard.py
+++ Dashboard.py
@@ -9,4 +9,17 @@ url = 'https://labdados.com/produtos'
 response = requests.get(url)
 dados = pd.DataFrame.from_dict(response.json())
 
+def formata_numero(valor, prefixo = ''):
+    for unidade in ['', 'mil']:
+        if valor <1000:
+            return f'{prefixo} {valor:.2f} {unidade}'
+        valor /= 1000
+    return f'{prefixo} {valor:.2f} milhões'
+
+coluna1, coluna2 = st.columns(2)
+with coluna1:
+    st.metric('Receita', formata_numero(dados['Preço'].sum(), 'R$'))
+with coluna2:
+    st.metric('Quantidade de vendas', formata_numero(dados.shape[0]))
+    
 st.dataframe(dados)
