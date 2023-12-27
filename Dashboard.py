69c5ec7a8bb9240b071512c8ca6fa21ec66eb3ec
Leandro Souza <leandro.souza.159@gmail.com>
2023-12-27 16:36:41 -0300

    Gerando gráficos de barra

--- Dashboard.py
+++ Dashboard.py
@@ -37,6 +37,8 @@ receita_mensal = (
 receita_mensal["Ano"] = receita_mensal["Data da Compra"].dt.year
 receita_mensal["Mês"] = receita_mensal["Data da Compra"].dt.month_name()
 
+receita_categorias = dados.groupby('Categoria do Produto')['Preço'].sum().sort_values(ascending=False)
+
 ## Gráficos
 fig_mapa_receita = px.scatter_geo(
     receita_estados,
@@ -63,12 +65,28 @@ fig_receita_mensal = px.line(
 
 fig_receita_mensal.update_layout(yaxis_title="Receita")
 
+fig_receita_estados = px.bar(receita_estados.head(),
+                                            x = 'Local da compra',
+                                            y = 'Preço',
+                                            text_auto = True,
+                                            title = 'Top estados')
+
+fig_receita_estados.update_layout(yaxis_title = 'Receita')
+
+fig_receita_categorias = px.bar(receita_categorias,
+                                text_auto = True,
+                                title = 'Receita por categoria')
+
+fig_receita_categorias.update_layout(yaxis_title = 'Receita')
+
 coluna1, coluna2 = st.columns(2)
 with coluna1:
     st.metric("Receita", formata_numero(dados["Preço"].sum(), "R$"))
     st.plotly_chart(fig_mapa_receita, use_container_width=True)
+    st.plotly_chart(fig_receita_estados, use_container_width = True)
 with coluna2:
     st.metric("Quantidade de vendas", formata_numero(dados.shape[0]))
     st.plotly_chart(fig_receita_mensal, use_container_width=True)
+    st.plotly_chart(fig_receita_categorias, use_container_width = True)
 
 st.dataframe(dados)
