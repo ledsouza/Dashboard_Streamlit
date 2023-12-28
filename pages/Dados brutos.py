09ebc5bb5d00f808be5d240bd04c706d027a0d0d
Leandro da Silva de Souza <leandro.souza.159@gmail.com>
2023-12-28 14:52:39 -0300

    Adicionado botão de download

--- pages/Dados brutos.py
+++ pages/Dados brutos.py
@@ -1,6 +1,17 @@
 import streamlit as st
 import requests
 import pandas as pd
+import time
+
+# Funções
+@st.cache_data
+def converte_csv(df):
+    return df.to_csv(index = False).encode('utf-8')
+
+def mensagem_sucesso():
+    sucesso = st.success('Arquivo baixado com sucesso!', icon = "✅")
+    time.sleep(5)
+    sucesso.empty()
 
 st.set_page_config(layout="wide")
 
@@ -56,3 +67,11 @@ dados_filtrados = dados_filtrados[colunas]
 st.dataframe(dados_filtrados)
 
 st.markdown(f'A tabela possui :blue[{dados_filtrados.shape[0]}] linhas e :blue[{dados_filtrados.shape[1]}] colunas')
+
+st.markdown('Escreva um nome para o arquivo')
+coluna1, coluna2 = st.columns(2)
+with coluna1:
+    nome_arquivo = st.text_input('', label_visibility = 'collapsed', value = 'dados')
+    nome_arquivo += '.csv'
+with coluna2:
+    st.download_button('Fazer o download da tabela em csv', data = converte_csv(dados_filtrados), file_name = nome_arquivo, mime = 'text/csv', on_click = mensagem_sucesso)
